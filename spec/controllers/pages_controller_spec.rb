d55d10ac8a028e4486ca09cb5a1ba8cf1c74a565
Luis Manotas <luis.manotas@koombea.com>
2014-07-18 11:15:29 -0500

    spec refactor

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
-  context "on index" do
-    let(:user) { FactoryGirl.create(:user) }
-
+  context "not signed in" do
     it "should render sign_in if user isn't logged" do
       get 'index'
       response.status.should == 302
       response.should redirect_to new_user_session_path
     end
+  end
 
-    it "should render index when the user is signed_in"
+  context "signed_in user" do
+    it "should render index when the user is signed_in" do
+      signed_in_user
+      get 'index'
+      response.should render_template('index')
+    end
   end
 end
--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -4,4 +4,14 @@ module Macros
       encrypted_password: Digest::SHA1.hexdigest("--123--somevalidpassword--"), 
       clearance_legacy_password: true)
   end
+
+  def rhomobile_user
+    u = FactoryGirl.create(:user)
+    u.confirm!
+    return u
+  end
+
+  def signed_in_user
+    sign_in(rhomobile_user)
+  end
 end
